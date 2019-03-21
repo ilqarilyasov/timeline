d3eb8cc382041f6fd237424bee58022eb1723ba8
Ilqar Ilyasov <ilqar_ilyasov@yahoo.com>
2019-03-20 20:50:27 -0500

    Implement add comment action and also presentCommentView action

--- Timeline/View Controllers/ImagePostDetailTableViewController.swift
+++ Timeline/View Controllers/ImagePostDetailTableViewController.swift
@@ -28,11 +28,30 @@ class ImagePostDetailTableViewController: UITableViewController {
         authorLabel.text = post.author.displayName
     }
     
-    // MARK: - Table view data source
+    // MARK: - Action
     
-    @IBAction func createComment(_ sender: Any) {
+    @IBAction func addComment(_ sender: Any) {
         
-        let alert = UIAlertController(title: "Add a comment", message: "Write your comment below:", preferredStyle: .alert)
+        presentActionSheet()
+    }
+    
+    private func presentActionSheet() {
+        let actionSheet = UIAlertController(title: "Comment Type", message: nil, preferredStyle: .actionSheet)
+        let text = UIAlertAction(title: "Text", style: .default) { (action) in
+            self.presentTextComment()
+        }
+        
+        let audio = UIAlertAction(title: "Audio", style: .default) { (action) in
+            self.presentAudioCommentView()
+        }
+        
+        actionSheet.addAction(text)
+        actionSheet.addAction(audio)
+        present(actionSheet, animated: true, completion: nil)
+    }
+    
+    private func presentTextComment() {
+        let alert = UIAlertController(title: "Add a text comment", message: "Write your comment below:", preferredStyle: .alert)
         
         var commentTextField: UITextField?
         
@@ -60,6 +79,33 @@ class ImagePostDetailTableViewController: UITableViewController {
         present(alert, animated: true, completion: nil)
     }
     
+    
+    // MARK: - Audio Comment View
+    
+    private func presentAudioCommentView() {
+        guard let commentView = Bundle.main.loadNibNamed("AudioCommentView", owner: self, options: nil)?.first as? AudioCommentView else { return }
+        
+        let blur = UIBlurEffect(style: .dark)
+        let blurView = UIVisualEffectView(effect: blur)
+        blurView.frame = view.bounds
+        blurView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+        view.addSubview(blurView)
+        
+        view.addSubview(commentView)
+        commentView.center.y = self.view.center.y
+        commentView.center.x = self.view.center.x
+        commentView.layer.cornerRadius = 20
+        commentView.layer.masksToBounds = true
+        
+        guard let cancelView = Bundle.main.loadNibNamed("CancelButtonView", owner: self, options: nil)?.first as? CancelButtonView else { return }
+        view.addSubview(cancelView)
+        cancelView.center.x = self.view.center.x
+        cancelView.frame.origin.y = self.view.bounds.midY + commentView.frame.size.height
+    }
+    
+    
+    // MARK: - Table view data source
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return (post?.comments.count ?? 0) - 1
     }
@@ -75,11 +121,12 @@ class ImagePostDetailTableViewController: UITableViewController {
         return cell
     }
     
+    
+    // MARK: - Properties
+    
     var post: Post!
     var postController: PostController!
     var imageData: Data?
-    
-    
     
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
